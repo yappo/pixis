58cf91db5ab7e5e6425d3818ea5fc47b8a213fe6
Daisuke Maki (lestrrat) <daisuke@endeworks.jp>
2009-02-28 16:15:46 +0900

    starting to add JPA schemas

---
+++ plugins/Pixis-Plugin-JPA/lib/Pixis/Schema/Master/JPAMember.pm
@@ -0,0 +1,69 @@
+
+package Pixis::Schema::Master::JPAMember;
+use strict;
+use warnings;
+use base 'DBIx::Class';
+
+
+__PACKAGE__->load_components("PK::Auto", "InflateColumn::DateTime", "Core");
+__PACKAGE__->table("pixis_jpa_member");
+__PACKAGE__->add_columns(
+    "id" => {
+        data_type => "INTEGER",
+        is_auto_increment => 1,
+        is_nullable => 0,
+        size => 32,
+    },
+    email => {
+        data_type => "VARCHAR",
+        is_nullable => 0,
+        size => 256,
+    },
+    firstname => {
+        data_type => "VARCHAR",
+        is_nullable => 0,
+        size => 256
+    },
+    lastname => {
+        data_type => "VARCHAR",
+        is_nullable => 0,
+        size => 256
+    },
+    country => {
+        data_type => "TEXT",
+        is_nullable => 1,
+    },
+    state   => {
+        data_type => "TEXT",
+        is_nullable => 1,
+    },
+    postal_code => { # won't bind to japanese addresses, for now
+        data_type => "TEXT",
+        is_nullable => 1,
+    },
+    address1 => { # when requiring addresses, this is required
+        data_type => "TEXT",
+        is_nullable => 1,
+    },
+    address2 => { # your town, whatever
+        data_type => "TEXT",
+        is_nullable => 1,
+    },
+    address3 => { # building name, room number, etc.
+        data_type => "TEXT",
+        is_nullable => 1,
+    },
+    modified_on => {
+        data_type => "TIMESTAMP",
+        is_nullable => 0,
+        default_value => \'NOW()',
+    },
+    created_on => {
+        data_type => "DATETIME",
+        is_nullable => 0,
+    },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+1;
---
+++ plugins/Pixis-Plugin-JPA/lib/Pixis/Schema/Master/JPAMembership.pm
@@ -0,0 +1,54 @@
+# $Id$
+
+package Pixis::Schema::Master::JPAMembership;
+use strict;
+use warnings;
+use base qw(DBIx::Class);
+use DateTime;
+
+__PACKAGE__->load_components("PK::Auto", "InflateColumn::DateTime", "Core");
+__PACKAGE__->table("pixis_jpa_membership");
+__PACKAGE__->add_columns(
+    "id" => {
+        data_type => "INTEGER",
+        is_auto_increment => 1,
+        is_nullable => 0,
+        size => 32,
+    },
+    name => {
+        data_type => "TEXT",
+        is_nullable => 0,
+    },
+    price => {
+        data_type => "INTEGER",
+        is_nullable => 0,
+        size => 8,
+    },
+    modified_on => {
+        data_type => "TIMESTAMP",
+        is_nullable => 0,
+        default_value => \'NOW()',
+    },
+    created_on => {
+        data_type => "DATETIME",
+        is_nullable => 0,
+    },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+sub populate_initial_data {
+    my ($class, $schema) = @_;
+
+    my $now = DateTime->now();
+    $schema->populate( 
+        JPAMembership => [
+            [ qw(name price created_on) ],
+            [ qw(JPA個人一般会員 5000), $now ],
+            [ qw(JPA個人学生会員 0), $now ],
+        ]
+    );
+}
+
+1;
+
