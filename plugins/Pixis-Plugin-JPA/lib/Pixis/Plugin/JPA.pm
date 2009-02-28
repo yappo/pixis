d55e65b3cb050db4267eba5f350534e70e570b03
Daisuke Maki (lestrrat) <daisuke@endeworks.jp>
2009-03-01 05:59:59 +0900

    add navigation

--- plugins/Pixis-Plugin-JPA/lib/Pixis/Plugin/JPA.pm
+++ plugins/Pixis-Plugin-JPA/lib/Pixis/Plugin/JPA.pm
@@ -5,6 +5,15 @@ use Moose;
 
 with 'Pixis::Plugin';
 
+has '+navigation' => (
+    default => sub {
+        {
+            text => "JPA",
+            url  => "/jpa"
+        }
+    }
+);
+
 has '+extra_api' => (
     default => sub { +[ qw(JPAMember) ] }
 );
