e744b4e607dd589286e8fb54cd27d493b48c9d6c
Daisuke Maki (lestrrat) <daisuke@endeworks.jp>
2009-02-28 17:46:38 +0900

    remove debug statement

--- core/lib/Pixis/Schema/Base/MySQL.pm
+++ core/lib/Pixis/Schema/Base/MySQL.pm
@@ -11,10 +11,6 @@ __PACKAGE__->mk_classdata('charset' => 'UTF8');
 sub sqlt_deploy_hook {
     my ($self, $sqlt_table) = @_;
 
-print <<EOM;
-    Running sql_deploy_hook!
-EOM
-
     $sqlt_table->extra->{mysql_table_type} = $self->engine;
     $sqlt_table->extra->{mysql_charset}    = $self->charset;
 }
