1a3115587d1ca08ad82f93873c06910febcd2d13
Daisuke Maki (lestrrat) <daisuke@endeworks.jp>
2009-03-05 08:27:03 +0900

    add helprs to run fastcgi

---
+++ core/t/lib/Test/Pixis/TestRunFastCGI.pm
@@ -0,0 +1,132 @@
+package Test::Pixis::TestRunFastCGI;
+use strict;
+use base qw(Apache::TestRun);
+use Path::Class::Dir;
+use Path::Class::File;
+use POSIX 'setsid';
+
+sub start {
+    my $self = shift;
+
+    if (! $ENV{NO_FASTCGI}) {
+        $self->start_fastcgi(@_);
+        print "started fastcgi\n";
+    }
+
+    $self->SUPER::start(@_);
+}
+
+sub start_fastcgi {
+    my $self = shift;
+
+    print "starting fastcgi\n";
+    my $home    = $ENV{MYAPP_HOME};
+    my $pidfile = $ENV{MYAPP_PIDFILE};
+    my $listen  = $ENV{MYAPP_SOCKET};
+
+    { # make sure that no previous instances are running
+        if (-f $pidfile) {
+            open (my $fh, '<', $pidfile) or
+                die "Could not open file $pidfile for reading: $!";
+
+            my $pid = <$fh>;
+            close $fh;
+
+            chomp $pid;    
+            if ($pid && kill 0 => $pid) {
+                print STDERR "PID $pid is still alive\n";
+                CORE::exit(1);
+            }
+            CORE::unlink $pidfile;
+        }
+    }
+
+
+    { # fork/exec to start fastcgi script
+        local $SIG{CHLD} = sub {
+            print STDERR "Child prematurely exited\n";
+            CORE::exit(1);
+        };
+
+        my $pid = fork();
+        if (! defined $pid) {
+            print STDERR "Could not fork: $!\n";
+            CORE::exit(1);
+        }
+
+        if ($pid) { # parent
+            # wait until the pid file we've requested 
+            my $timeout = time + 60;
+            my $ok = 0;
+            while (! -f $pidfile && $timeout > time() ) {
+                sleep 1;
+            }
+
+            if (! -f $pidfile) {
+                print STDERR "FastCGI script did not start. Please check your script\n";
+                CORE::exit(1);
+            }
+        } else {
+            $ENV{MYAPP_HOME}    = $home;
+            $ENV{MYAPP_PIDFILE} = $pidfile;
+            $ENV{MYAPP_SOCKET}  = $listen;
+            setsid() or die "Could not setsid";
+            my $cmd = Path::Class::File->new($home, 'script', 'run.sh')->absolute->stringify;
+            exec($cmd) or die "Failed to run script: $!";
+            exit 1;
+        }
+    }
+}
+
+sub install_sighandlers {
+    my $self = shift;
+    $self->SUPER::install_sighandlers(@_);
+
+    my $old = $SIG{__DIE__}; # capture this sucker
+    $SIG{__DIE__} = sub {
+        return unless $_[0] =~ /^Failed/i; #dont catch Test::ok failures
+
+        $self->stop_fastcgi;
+        $old->(@_);
+    };
+}
+
+sub stop {
+    my $self = shift;
+    $self->stop_fastcgi;
+    $self->SUPER::stop(@_);
+}
+
+sub stop_fastcgi {
+    my $self = shift;
+
+    my $home    = $ENV{MYAPP_HOME};
+    my $pidfile = $ENV{MYAPP_PIDFILE};
+    my $listen  = $ENV{MYAPP_SOCKET};
+
+    if (-f $pidfile) {
+        open (my $fh, '<', $pidfile) or
+            die "Could not open file $pidfile for reading: $!";
+
+        my $pid = <$fh>;
+        close $fh;
+
+        chomp $pid;
+
+        if ($pid) {
+            # wait until the pid file we've killed it
+            my $timeout = time + 60;
+            while ( kill TERM => $pid && $timeout > time() ) {
+                sleep 1;
+            }
+
+            if (kill TERM => $pid)  {
+                print STDERR "PID $pid is still alive\n";
+            }
+            CORE::unlink $pidfile;
+        }
+    }
+}
+
+1;
+
