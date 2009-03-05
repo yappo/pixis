9829a612130ee33a837dff7870210c579a6e7e00
Daisuke Maki (lestrrat) <daisuke@endeworks.jp>
2009-03-05 10:30:25 +0900

    more test stuff

--- core/lib/Pixis/API/Member.pm
+++ core/lib/Pixis/API/Member.pm
@@ -46,6 +46,9 @@ sub create {
 sub activate {
     my ($self, $args) = @_;
 
+    $args->{token} or die "no token";
+    $args->{email} or die "no email";
+
     my $schema = Pixis::Registry->get(schema => 'master');
     $schema->txn_do( sub {
         my ($self, $args) = @_;
@@ -59,8 +62,9 @@ sub activate {
             $member->activation_token(undef);
             $member->is_active(1);
             $member->update;
+            return $self->find($member->id);
         }
-        return $member;
+        return ();
     }, $self, $args );
 }
 
--- core/t/lib/Test/Pixis.pm
+++ core/t/lib/Test/Pixis.pm
@@ -1,7 +1,15 @@
 
 package Test::Pixis;
 use MooseX::Singleton;
+use Cache::Memcached;
 use Config::Any;
+use Digest::SHA1 ();
+
+has 'cache' => (
+    is => 'rw',
+    isa => 'Object',
+    lazy_build => 1,
+);
 
 has 'config' => (
     is => 'rw',
@@ -15,6 +23,17 @@ has 'configfile' => (
     lazy_build => 1
 );
 
+sub _build_cache {
+    my $self = shift;
+    my $config = $self->config;
+    my $cache_config = $config->{'Cache::Memcached'} || {};
+    if ($ENV{MEMCACHED_SERVER}) {
+        $cache_config->{servers} = [ $ENV{MEMCACHED_SERVER} ];
+    }
+    $cache_config->{namespace} ||= Digest::SHA1::sha1_hex($<, {}, $$, rand(), time());
+    return Cache::Memcached->new($cache_config);
+}
+
 sub _build_configfile {
     return $ENV{MYAPP_CONFIG} || 't/conf/pixis_test.yaml';
 }
@@ -27,6 +46,43 @@ sub _build_config {
         use_ext => 1,
     });
     return $cfg->[0]->{$filename};
+}
+
+sub has_cache {
+    my $self = shift;
+
+    return $ENV{MEMCACHED_SERVER} ?  1 : ();
+
+    my $config = $self->config;
+    my $cache_config = $config->{'Cache::Memcached'};
+    return $cache_config ? 1 : ()
+}
+
+sub make_schema {
+    my ($self, $type) = @_;
+
+    my $class = "Pixis::Schema::$type";
+    my $config_key = "Schema::$type";
+
+    my $config = $self->config;
+    Class::MOP::load_class($class);
+    return $class->connection(@{$config->{$config_key}->{connect_info}});
+}
+
+sub make_api {
+    my ($self, $type) = @_;
+
+    my $class = "Pixis::API::$type";
+    my $config_key = "Schema::$type";
+
+    my $config = $self->config;
+    Class::MOP::load_class($class);
+
+    my $api_config = $config->{$config_key} || {};
+    if ($self->has_config) {
+        $api_config->{cache} = $self->cache;
+    }
+    return $class->new(%$api_config);
 }
 
 __PACKAGE__->meta->make_immutable;
