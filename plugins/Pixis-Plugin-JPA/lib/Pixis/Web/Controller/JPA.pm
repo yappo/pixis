cd20d80f2cd3de7422d101192d27e3b35a590ee0
Daisuke Maki (lestrrat) <daisuke@endeworks.jp>
2009-03-12 14:57:45 +0900

    add board

--- plugins/Pixis-Plugin-JPA/lib/Pixis/Web/Controller/JPA.pm
+++ plugins/Pixis-Plugin-JPA/lib/Pixis/Web/Controller/JPA.pm
@@ -3,7 +3,9 @@ package Pixis::Web::Controller::JPA;
 use strict;
 use base qw(Catalyst::Controller);
 
-sub index :Index :Args(0) {}
+sub index     :Index :Args(0) {}
 sub poweredby :Local :Args(0) {}
+sub board     :Local :Args(0) {}
+sub sponsors  :Local :Args(0) {}
 
 1;
