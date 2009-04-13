cd2fce6ec742b18aa89c99fb33db0715f725f2cb
Daisuke Maki (lestrrat) <daisuke@endeworks.jp>
2009-04-13 11:14:08 +0900

    redo schema stuff

--- core/lib/Pixis/Plugin/Core.pm
+++ core/lib/Pixis/Plugin/Core.pm
@@ -9,13 +9,21 @@ before register => sub {
     my ($self, $c) = @_;
 
     my $config = $c->config();
+
+    my $registry = Pixis::Registry->instance;
+    foreach my $name qw(Master) {
+        my $schema_config = $config->{"Schema::$name"};
+        my $module = "Pixis::Schema::$name";
+        Class::MOP::load_class($module);
+        my $schema = $module->connection( @{$schema_config->{connect_info}} );
+        $registry->set("schema" => $name => $schema);
+    }
+
     my @list;
     foreach my $name qw(Member MemberAuth MemberRelationship Order Payment::Paypal Payment::Transaction) {
         my $api_config = $config->{"API::$name"} || {};
         my $module     = "Pixis::API::$name";
         Class::MOP::load_class($module);
-use Data::Dumper;
-print STDERR "$module -> ", Dumper($api_config);
         my $api = $module->new(%$api_config);
         push @list, $api;
     }
